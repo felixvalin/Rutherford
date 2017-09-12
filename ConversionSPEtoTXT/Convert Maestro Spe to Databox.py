2fe03662ef052e63c9090dbfc72d985a50aa14f4
felixvalin <felix.valin@mail.mcgill.ca>
2017-09-12 10:16:44 -0400

    New converted files

---
+++ ConversionSPEtoTXT/Convert Maestro Spe to Databox.py
@@ -0,0 +1,42 @@
+import glob
+import spinmob
+
+paths = glob.glob("*.Spe")
+
+# loop over the paths
+for path in paths:
+    
+    print("Converting " + path + " ...")
+    
+    # read all the lines in the file
+    lines = spinmob.fun.read_lines(path)
+    
+    # make a databox for saving
+    d = spinmob.data.databox()    
+    d['Channel'] = []
+    d['Counts']  = []    
+    
+    # search for the data
+    n = 0
+    i = 0
+    for i in range(len(lines)):
+        
+        # data starts with a space
+        if lines[i][0] == ' ': 
+            d.append_data_point([n, int(lines[i].strip())])
+            n += 1
+    
+        # get info
+        elif lines[i][0]=='$':
+
+            # get the hkey and value
+            hkey = lines[i][1:].strip()
+            value= lines[i+1].strip()
+            d.insert_header(hkey, value)
+    
+    # get the new path
+    s = path.split('.')
+    s[-1] = 'txt'
+    s='.'.join(s)
+    d.save_file(path=('../DataConverted/%s' %s), force_overwrite=True)
+#raw_input('<enter>')
---
+++ ConversionSPEtoTXT/README.md
@@ -0,0 +1,2 @@
+
+Just add the .spe files to this folder, and then run the python script in order to convert them and transfer them into '../DataConverted/'.
