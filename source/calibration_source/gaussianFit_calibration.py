5041d545d6f5b4745aec8fa607a99d51648d8f67
felixvalin <felix.valin@mail.mcgill.ca>
2017-09-21 10:58:28 -0400

    coarsen americium

--- source/calibration_source/gaussianFit_calibration.py
+++ source/calibration_source/gaussianFit_calibration.py
@@ -17,11 +17,14 @@ for path in paths:
     
     Gaussianfitter = s.data.fitter('a*exp(-(x-x0)**2/w**2)+b', 'a=1, b=0, x0=1000, w=25', ymin=4)
     d = s.data.load(path)
+    d[0]=s.fun.coarsen_array(d[0], level=2, method='mean')
+    d[1]=s.fun.coarsen_array(d[1], level=2, method='mean')
     Gaussianfitter.set_data(xdata=d[0], ydata=d[1], eydata=np.sqrt(d[1]))
     print("CLICK THE PEAK!!")
     click_x, click_y = Gaussianfitter.ginput()[0]
-    Gaussianfitter(a=click_y, x0=click_x, xmin=click_x-200, xmax=click_x+200, ymin=0.5)
+    Gaussianfitter(a=click_y, x0=click_x, xmin=click_x-200, xmax=click_x+200, ymin=1)
     Gaussianfitter.fit()
+    print(Gaussianfitter.reduced_chi_squareds)
     Gaussianfitter.ginput()
 #    fitVolts = np.int(path.split('.')[0].split('_')[2][:-1])
 #    plt.savefit("wrongFit.png")
